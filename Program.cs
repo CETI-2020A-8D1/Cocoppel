c0e3b0ee8960350728a5aab09fb95914e1537336
Andrea-Gaspar <62579844+Andrea-Gaspar@users.noreply.github.com>
2020-03-29 13:27:05 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,15 +9,47 @@ namespace ApiBanco
         int saldoLimite = 20000;
         bool MaximoCobro(TarjetaCredito credit) //Si el credito es menor al limite de saldo devuelve verdadero si no, devuelve falso
         {
-            if(credit.Saldo < saldoLimite)
+            if(credit.Saldo < Limite)
             {
                 return true;
             }
 
             return false;
         }
-        
-        
+        bool validarTarjeta(TarjetaDebito debit)
+        {
+            if(debit.NumeroDeTarjeta.leght != 16)
+            {
+                return false;
+            }
+            if(debit.FechaVencimiento.Date<DateTime.Today.Date)
+            {
+                return false;
+            }
+            if (debit.Ccv.leght != 3)
+            {
+                return false;
+            }
+            return true;
+        }
+        bool validarTarjeta(TarjetaCredito credit)
+        {
+            if (credit.NumeroDeTarjeta.leght != 16)
+            {
+                return false;
+            }
+            if (credit.FechaVencimiento.Date < DateTime.Today.Date)
+            {
+                return false;
+            }
+            if (credit.Ccv.leght != 3)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
         static void Main(string[] args)
         {
             
