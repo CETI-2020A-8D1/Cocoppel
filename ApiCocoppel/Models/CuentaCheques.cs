8bcfba143d2018b7e47dd73e22ddeefa7116c748
CAMELOT3 <a16300336@ceti.mx>
2020-04-04 18:28:46 -0600

    Mapeo
    Mapeo y Conexion

---
+++ ApiCocoppel/Models/CuentaCheques.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApiCocoppel.Models
+{
+    public partial class CuentaCheques
+    {
+        public int IdnumeroDeCuenta { get; set; }
+        public int Idusuario { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+
+        public Usuario IdusuarioNavigation { get; set; }
+        public TarjetaDebito TarjetaDebito { get; set; }
+    }
+}
---
+++ ApiCocoppel/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApiCocoppel.Models
+{
+    public partial class Usuario
+    {
+        public Usuario()
+        {
+            CuentaCheques = new HashSet<CuentaCheques>();
+            LineaCredito = new HashSet<LineaCredito>();
+        }
+
+        public int Idusuario { get; set; }
+        public DateTime FechaAfiliacion { get; set; }
+        public int PuntajeCredito { get; set; }
+
+        public ICollection<CuentaCheques> CuentaCheques { get; set; }
+        public ICollection<LineaCredito> LineaCredito { get; set; }
+    }
+}
