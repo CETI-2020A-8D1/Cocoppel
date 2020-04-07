e5de07f1f29d1495877348ce469ba9eb52398eab
Cristopher Ceja <a16300065@ceti.mx>
2020-04-06 19:59:33 -0500

    Reagregar Cocoppel.sql

--- DTO/TarjetaCreditoDTO.cs
+++ DTO/TarjetaCreditoDTO.cs
@@ -6,13 +6,9 @@ namespace Cocoppel.DTO
     public partial class TarjetaCreditoDTO
     {
         public int IdlineaCredito { get; set; }
-        public string EntidadEmisora { get; set; }
-        public string Titular { get; set; }
         public string Numero { get; set; }
         public DateTime FechaCaducidad { get; set; }
         public int Cvv { get; set; }
-        public int Nip { get; set; }
-        public string MarcaInternacional { get; set; }
 
         public virtual LineaCreditoDTO IdlineaCreditoNavigation { get; set; }
     }
